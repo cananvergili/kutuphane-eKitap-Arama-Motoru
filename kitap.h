477943af6fafc489439b568f8f37319b5a132e43
cananvergili <cananvergili21@gmail.com>
2026-01-06 13:14:24 +0300

    Update kitap.h

--- kitap.h
+++ kitap.h
@@ -1,7 +1,7 @@
 #ifndef KITAP_H
 #define KITAP_H
 
-#define MAKS_KITAP 500
+#define MAKS_KITAP 200
 
 typedef struct {
     int id;
@@ -14,12 +14,9 @@ typedef struct {
 extern Kitap kitaplar[MAKS_KITAP];
 extern int kitap_sayisi;
 
-// mevcutlar
 void tum_kitaplari_listele(void);
 void ornek_kitaplari_yukle(void);
 
-// yeni eklenenler
-int  kitap_id_var_mi(int id);
 void kullanici_kitap_ekle(void);
 void kullanici_kitap_sil(void);
 
